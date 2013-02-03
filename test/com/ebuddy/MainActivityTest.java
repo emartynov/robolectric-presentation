b7dbb34fbdbaebd34b31a9b8ce18e8f4269387a8
Eugen Martynov <eugen.martynov@gmail.com>
2013-02-03 10:20:22 +0100

    Specified source and test directory. Minor test modification

--- test/com/ebuddy/MainActivityTest.java
+++ test/com/ebuddy/MainActivityTest.java
@@ -1,15 +1,20 @@
 package com.ebuddy;
 
+import org.junit.Before;
+import org.junit.Test;
+
 public class MainActivityTest
 {
     private MainActivity activity;
 
+    @Before
     public void setUp () throws Exception
     {
         activity = new MainActivity();
         activity.onCreate( null );
     }
 
+    @Test
     public void checkInitialTexts () throws Exception
     {
     }
