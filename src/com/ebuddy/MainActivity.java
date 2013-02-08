456074ed0d69bbbc8fcf6a8e97acc637f6a1d38d
Eugen Martynov <eugen.martynov@gmail.com>
2013-02-08 09:05:04 +0100

    Final code after demos

--- src/com/ebuddy/MainActivity.java
+++ src/com/ebuddy/MainActivity.java
@@ -1,6 +1,5 @@
 package com.ebuddy;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -8,12 +7,19 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import com.squareup.otto.Bus;
+import roboguice.activity.RoboActivity;
 
-public class MainActivity extends Activity implements View.OnClickListener
+import javax.inject.Inject;
+
+public class MainActivity extends RoboActivity implements View.OnClickListener
 {
 
     EditText editUrl;
     Button downloadButton;
+
+    @Inject
+    Bus bus;
 
     /**
      * Called when the activity is first created.
@@ -21,6 +27,8 @@ public class MainActivity extends Activity implements View.OnClickListener
     @Override
     public void onCreate ( Bundle savedInstanceState )
     {
+
+
         super.onCreate( savedInstanceState );
         setContentView( R.layout.main );
 
@@ -28,11 +36,21 @@ public class MainActivity extends Activity implements View.OnClickListener
         downloadButton = (Button) findViewById( R.id.downloadButton );
 
         downloadButton.setOnClickListener( this );
+
+        bus.register( this );
     }
 
     @Override
     public void onClick ( View view )
     {
+        startDownload();
+    }
+
+    private void startDownload ()
+    {
+        //download
+        downloadButton.setEnabled( false );
+        editUrl.setEnabled( false );
     }
 
     @Override
@@ -47,12 +65,24 @@ public class MainActivity extends Activity implements View.OnClickListener
     @Override
     public boolean onPrepareOptionsMenu ( Menu menu )
     {
-        return false;
+        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected ( MenuItem item )
     {
+        if (item.getItemId() == R.id.cancel)
+        {
+            stopDownload();
+
+        }
         return super.onOptionsItemSelected( item );
+    }
+
+    private void stopDownload ()
+    {
+        //cancel download
+        downloadButton.setEnabled( true );
+        editUrl.setEnabled( true );
     }
 }
--- test/com/ebuddy/MainActivityTest.java
+++ test/com/ebuddy/MainActivityTest.java
@@ -1,13 +1,22 @@
 package com.ebuddy;
 
+import android.view.Menu;
+import com.google.inject.AbstractModule;
+import com.squareup.otto.Bus;
+import com.xtremelabs.robolectric.tester.android.view.TestMenu;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
-public class MainActivityTest
+@RunWith ( InjectingTestRunner.class)
+public class MainActivityTest extends InjectedTest
 {
     private MainActivity activity;
+    private Bus bus = mock( Bus.class );
 
     @Before
     public void setUp () throws Exception
@@ -21,6 +30,50 @@ public class MainActivityTest
     {
         assertThat( activity.downloadButton.getText() ).isEqualTo(
                                            activity.getResources().getString( R.string.download ) );
+    }
+
+    @Test
+    public void disableControlWhenDownload ()
+    {
+        activity.downloadButton.performClick();
+
+        assertThat( activity.downloadButton.isEnabled() ).isFalse();
+        assertThat( activity.editUrl.isEnabled() ).isFalse();
+    }
+
+    @Test
+    public void enableControlsWhenCancel ()
+    {
+        activity.downloadButton.performClick();
+
+        Menu menu = new TestMenu(  );
+        activity.onCreateOptionsMenu( menu );
+        activity.onPrepareOptionsMenu( menu );
+
+        activity.onOptionsItemSelected( menu.findItem( R.id.cancel ) );
+
+        assertThat( activity.downloadButton.isEnabled() ).isTrue();
+        assertThat( activity.editUrl.isEnabled() ).isTrue();
+    }
+
+    @Test
+    public void registerToBusWhenCreated ()
+    {
+        verify( bus ).register(activity);
+    }
+
+
+    @Override
+    public AbstractModule getTestModule ()
+    {
+        return new AbstractModule()
+        {
+            @Override
+            protected void configure ()
+            {
+                bind( Bus.class ).toInstance( bus );
+            }
+        };  //To change body of implemented methods use File | Settings | File Templates.
     }
 }
 
